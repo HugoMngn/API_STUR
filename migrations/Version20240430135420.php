8c2fa555fdedb98f828da7f731b679954a01f35c
Drac0fire <166018262+HugoMngn@users.noreply.github.com>
2024-04-30 16:22:15 +0200

    usercontroller verifier

--- migrations/Version20240430135420.php
+++ migrations/Version20240430135420.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240430132627 extends AbstractMigration
+final class Version20240430135420 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -24,8 +24,9 @@ final class Version20240430132627 extends AbstractMigration
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, parent_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_64C19C1727ACA70 (parent_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE diary (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, user_pseudonym VARCHAR(255) NOT NULL, open TINYINT(1) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', duration INT NOT NULL, UNIQUE INDEX UNIQ_917BEDE2A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE etude (id INT AUTO_INCREMENT NOT NULL, filiere_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_1DDEA924180AA129 (filiere_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, pseudonyme VARCHAR(255) NOT NULL, email_adress VARCHAR(255) NOT NULL, age INT NOT NULL, gender VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, pseudonyme VARCHAR(255) NOT NULL, email_adress VARCHAR(255) DEFAULT NULL, age INT NOT NULL, gender VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_etude (user_id INT NOT NULL, etude_id INT NOT NULL, INDEX IDX_FF1C527CA76ED395 (user_id), INDEX IDX_FF1C527C47ABD362 (etude_id), PRIMARY KEY(user_id, etude_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE activity ADD CONSTRAINT FK_AC74095AE020E47A FOREIGN KEY (diary_id) REFERENCES diary (id)');
         $this->addSql('ALTER TABLE activity ADD CONSTRAINT FK_AC74095A12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
         $this->addSql('ALTER TABLE category ADD CONSTRAINT FK_64C19C1727ACA70 FOREIGN KEY (parent_id) REFERENCES category (id)');
@@ -51,5 +52,6 @@ final class Version20240430132627 extends AbstractMigration
         $this->addSql('DROP TABLE etude');
         $this->addSql('DROP TABLE `user`');
         $this->addSql('DROP TABLE user_etude');
+        $this->addSql('DROP TABLE messenger_messages');
     }
 }
--- src/Service/UserJsonFormatter.php
+++ src/Service/UserJsonFormatter.php
@@ -2,6 +2,7 @@
 
 namespace App\Service;
 
+use App\Entity\Etude;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\User;
 
@@ -17,29 +18,54 @@ class UserJsonFormatter
     public function getUserDetails(int $userId): ?array
     {
         $user = $this->entityManager->getRepository(User::class)->find($userId);
-
+    
         if (!$user) {
             return null;
         }
-
+        $etudesDetails = [];
+        foreach ($user->getEtudes() as $etude) {
+            $etudesDetails[] = [
+                'id' => $etude->getId(),
+                'name' => $etude->getName(),
+            ];
+        }
+    
         return [
             'id' => $user->getId(),
             'pseudonyme' => $user->getPseudonyme(),
-            // Add other user details as needed
+            'age' => $user->getAge(),
+            'email' => $user->getEmailAdress(),
+            'etudes' => $etudesDetails,
         ];
     }
+    
 
     public function createUser(array $data): ?array
     {
         $user = new User();
         $user->setPseudonyme($data['pseudonyme']);
-        // Set other user properties as needed
-
+        $user->setEmailAdress($data['email_address']);
+        $user->setAge($data['age']);
+        $user->setGender($data['gender']);
+    
+        if (isset($data['etudes'])) {
+            $etudeIds = $data['etudes'];
+            foreach ($etudeIds as $etudeId) {
+                $etude = $this->entityManager->getRepository(Etude::class)->find($etudeId);
+                if ($etude) {
+                    $user->addEtude($etude);
+                } else {
+                    return ['error' => 'ID invalide pour une étude'];
+                }
+            }
+        }
+    
         $this->entityManager->persist($user);
         $this->entityManager->flush();
-
+    
         return $this->getUserDetails($user->getId());
     }
+    
 
     public function updateUser(int $userId, array $data): ?array
     {
@@ -50,7 +76,9 @@ class UserJsonFormatter
         }
 
         $user->setPseudonyme($data['pseudonyme']);
-        // Update other user properties as needed
+        $user->setAge($data['Age']);
+        $user->setEmailAdress($data['email']);
+        $user->setGender($data['gender']);
 
         $this->entityManager->flush();
 
